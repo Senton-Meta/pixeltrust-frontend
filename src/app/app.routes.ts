5b3469df1cc186fb5ffe7cc96ea5584844687ace
Stepan Berkunov <studiorain27@gmail.com>
2023-10-09 20:50:02 +0300

    feat(core): add basic components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "../pages/home-page/home-page.component";
+import {NotFoundComponent} from "../pages/not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent}
+];
