82546c7bb370f30c2bd8105a05f24ca94c0e4911
Stepan Berkunov <studiorain27@gmail.com>
2023-10-09 21:27:45 +0300

    feat: add ui library and css reset

--- src/pages/home-page/home-page.component.ts
+++ src/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
