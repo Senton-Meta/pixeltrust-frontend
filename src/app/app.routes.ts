18e1b3cd1041faac6d7d0e7d4d794128b2605ac8
Stepan Berkunov <studiorain27@gmail.com>
2023-10-10 00:12:34 +0300

    feat(architecture): add basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from "../pages/home-page/home-page.component";
 import {NotFoundComponent} from "../pages/not-found/not-found.component";
+import {PaymentsPageComponent} from "../pages/payments-page/payments-page.component";
+import {HistoryPageComponent} from "../pages/history-page/history-page.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
+  { path: 'payments', component: PaymentsPageComponent },
+  { path: 'history', component: HistoryPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent}
 ];
---
+++ src/pages/payments-page/payments-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-payments-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payments-page.component.html',
+  styleUrls: ['./payments-page.component.scss']
+})
+export class PaymentsPageComponent {
+
+}
